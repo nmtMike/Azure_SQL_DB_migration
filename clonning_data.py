a696c634a5d7f8374689073c1f07b46b398a698f
nmtMike <108139735+nmtMike@users.noreply.github.com>
2022-09-19 16:31:40 +0700

    add some tables

--- clonning_data.py
+++ clonning_data.py
@@ -27,35 +27,47 @@ import sqlite3
 conn = sqlite3.connect(r"D:\NMT\OneDrive\Viettravel Airline\Database\VTA_RM.db")
 c = conn.cursor()
 
-# cargo table
+# cargo
 cargo = pd.read_sql_query('SELECT * FROM cargo', conn)
 cargo['flight_date'] = pd.to_datetime(cargo['flight_date'])
 cargo['modified_time'] = pd.to_datetime(cargo['modified_time'])
 cargo.to_sql('cargo', az_db_conn, if_exists='replace', index=False)
 
-# dim_agent table
+# dim_agent
 dim_agent = pd.read_sql_query('SELECT * FROM dim_agent', conn)
 dim_agent['modified_time'] = pd.to_datetime(dim_agent['modified_time'])
 dim_agent.to_sql('dim_agent', az_db_conn, if_exists='replace', index=False)
 
-# dim_calendar table
+# dim_calendar
 dim_calendar = pd.read_sql_query('SELECT * FROM dim_calendar', conn)
 dim_calendar['Date'] = pd.to_datetime(dim_calendar['Date'])
 dim_calendar['modified_time'] = pd.to_datetime(dim_calendar['modified_time'])
 dim_calendar.to_sql('dim_calendar', az_db_conn, if_exists='replace', index=False)
 
-# dim_fare_code table
+# dim_fare_code
 dim_fare_code = pd.read_sql_query('SELECT * FROM dim_fare_code', conn)
 dim_fare_code['valid_date'] = pd.to_datetime(dim_fare_code['valid_date'])
 dim_fare_code['modified_time'] = pd.to_datetime(dim_fare_code['modified_time'])
 dim_fare_code.to_sql('dim_fare_code', az_db_conn, if_exists='replace', index=False)
 
-# dim_routes table
+# dim_routes
 dim_routes = pd.read_sql_query('SELECT * FROM dim_routes', conn)
 dim_routes['modified_time'] = pd.to_datetime(dim_routes['modified_time'])
 dim_routes.to_sql('dim_routes', az_db_conn, if_exists='replace', index=False)
 
-# flight_type table
+# dim_slot_time
+dim_slot_time = pd.read_sql_query('SELECT * FROM dim_slot_time', conn)
+dim_slot_time['modified_time'] = pd.to_datetime(dim_slot_time['modified_time'])
+dim_slot_time.to_sql('dim_slot_time', az_db_conn, if_exists='replace', index=False)
+
+# flight_type
 flight_type = pd.read_sql_query('SELECT * FROM flight_type', conn)
 flight_type['modified_time'] = pd.to_datetime(flight_type['modified_time'])
 flight_type.to_sql('flight_type', az_db_conn, if_exists='replace', index=False)
+
+# flown_aircraft_leg
+flown_aircraft_leg = pd.read_sql_query('SELECT * FROM flown_aircraft_leg', conn)
+flown_aircraft_leg['modified_time'] = pd.to_datetime(flown_aircraft_leg['modified_time'])
+flown_aircraft_leg['std_lt'] = pd.to_datetime(flown_aircraft_leg['std_lt'])
+flown_aircraft_leg['sta_lt'] = pd.to_datetime(flown_aircraft_leg['sta_lt'])
+
